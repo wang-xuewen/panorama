53d97f0b6bcdc1bcc734ed82bf08153bf0baf145
纵歌 <xw.wang@catontechnology.com>
2025-06-23 17:50:10 +0800

    sqlite 增加 create table

--- src/use_sample.rs
+++ src/use_sample.rs
@@ -1,23 +1,59 @@
 use crate::common::global;
-use anyhow::Result;
+use anyhow::{bail, Ok, Result};
 use log::{error, info, warn};
 
 pub fn use_sqlite() -> Result<()> {
+    create_table()?;
+
     insert_data("aaa", "aaa_value")?;
     let result = query_data("aaa")?;
     info!("query_data result:{}", result);
     Ok(())
 }
 
-// 写入数据
-fn insert_data(key: &str, value: &str) -> Result<()> {
+fn create_table() -> Result<()> {
     let db = global::get_global_db()?;
-    let mut conn = db.lock().unwrap_or_else(|poisoned| {
+    let mut db_obj = db.lock().unwrap_or_else(|poisoned| {
         warn!("⚠️Mutex锁中毒，强制恢复访问");
         poisoned.into_inner() // 从中毒状态恢复数据访问
     });
 
-    conn.conn
+    // 可以使用 CREATE TABLE IF NOT EXISTS users ？
+
+    if let Some(conn) = db_obj.conn.as_mut() {
+        let mut stmt = conn.prepare(
+            "SELECT count(*) FROM sqlite_master
+            WHERE type='table' AND name='table_test';",
+        )?;
+        let count: i64 = stmt.query_row([], |row| row.get(0))?;
+
+        if count == 0 {
+            conn.execute(
+                "CREATE TABLE table_test (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        key TEXT NOT NULL,
+                        value TEXT NULL
+                    )",
+                [],
+            )?;
+        }
+
+        Ok(())
+    } else {
+        bail!("Connection is None")
+    }
+}
+
+// 写入数据
+fn insert_data(key: &str, value: &str) -> Result<()> {
+    let db = global::get_global_db()?;
+    let mut db_obj = db.lock().unwrap_or_else(|poisoned| {
+        warn!("⚠️Mutex锁中毒，强制恢复访问");
+        poisoned.into_inner() // 从中毒状态恢复数据访问
+    });
+
+    db_obj
+        .conn
         .as_mut()
         .ok_or_else(|| {
             error!("取得rusqlite::Connection 可变访问出错");
@@ -37,10 +73,17 @@ fn insert_data(key: &str, value: &str) -> Result<()> {
 fn query_data(key: &str) -> Result<String> {
     let db = global::get_global_db()?;
     let conn = db.lock().unwrap();
-    let value: String = conn.conn.as_ref().unwrap().query_row(
-        "SELECT value FROM table_test WHERE key = ?1",
-        [key],
-        |row| row.get(0),
-    )?;
+    let value: String = conn
+        .conn
+        .as_ref()
+        .ok_or_else(|| {
+            error!("取得rusqlite::Connection 出错");
+            rusqlite::Error::InvalidQuery
+        })?
+        .query_row(
+            "SELECT value FROM table_test WHERE key = ?1",
+            [key],
+            |row| row.get(0),
+        )?;
     Ok(value)
 }
