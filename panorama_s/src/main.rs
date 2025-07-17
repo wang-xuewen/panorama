e4f3e684263499a6f2adf7b062326d4d12667f99
纵歌 <xw.wang@catontechnology.com>
2025-07-17 17:52:14 +0800

    增加graceful shutdown

--- panorama_s/src/main.rs
+++ panorama_s/src/main.rs
@@ -6,19 +6,16 @@ use crate::common::global;
 use crate::use_sqlite::use_sqlite;
 use crate::web_server::web_server_main;
 use anyhow::Result;
-
 use log::{error, info};
 use log4rs;
+use rust_utils::graceful_shutdown;
 
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
 
     // 初始化日志系统
-    if let Err(e) = log4rs::init_file(
-        global::LOG4RS_YAML_PATH,
-        Default::default(),
-    ) {
+    if let Err(e) = log4rs::init_file(global::LOG4RS_YAML_PATH, Default::default()) {
         eprintln!("init log4rs Error: {}", e);
     }
     info!("init log4rs ok.");
@@ -31,9 +28,15 @@ async fn main() {
         Ok(_) => info!("[sqlite] use_sqlite ok."),
         Err(e) => error!("[sqlite] use_sqlite failed: {}", e),
     }
-    if let Err(e) = web_server_main::run_server().await {
-        error!("Server error: {}", e);
+
+    match graceful_shutdown().await {
+        Ok(()) => println!("Shutdown successful"),
+        Err(e) => eprintln!("Shutdown failed: {}", e),
     }
+
+    // if let Err(e) = web_server_main::run_server().await {
+    //     error!("Server error: {}", e);
+    // }
 }
 
 fn init() -> Result<()> {
